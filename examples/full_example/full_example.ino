2460b7912c171181b728a42259a20d6c902731ae
nthnn <nathanneisip@gmail.com>
2023-07-31 20:39:39 +0800

    Updated full example.

--- examples/full_example/full_example.ino
+++ examples/full_example/full_example.ino
@@ -7,19 +7,17 @@
 #define OUT_PIN 16
 
 TCS3200 tcs3200(S0_PIN, S1_PIN, S2_PIN, S3_PIN, OUT_PIN);
-String color_indices[] = {"Red", "Green", "Blue", "White", "Black"};
+String color_indices[] = {"Red", "Green", "Blue"};
 
 RGBColor color_values[] = {
-  {255, 0, 0, 0},
-  {0, 255, 0, 0},
-  {0, 0, 255, 0},
-  {0, 0, 0, 255},
-  {0, 0, 0, 0}
+  {255, 0, 0},
+  {0, 255, 0},
+  {0, 0, 255},
 };
 
 void setup() {
   tcs3200.begin();
-  tcs3200.frequency_scaling(TCS3200_OFREQ_100P);
+  tcs3200.frequency_scaling(TCS3200_OFREQ_20P);
 
   Serial.begin(115200);
   Serial.println("TCS3200 Full Example");
@@ -67,9 +65,7 @@ void loop() {
 
   Serial.println("Chroma: " + String(tcs3200.get_chroma()));
   Serial.println("Dominant color: " + color_indices[tcs3200.get_rgb_dominant_color()]);
-
-  //auto nearest = tcs3200.nearest_color<String>(color_indices, color_values, 5);
-  //Serial.println("Nearest color: ");
+  Serial.println("Nearest color: " + tcs3200.nearest_color<String>(color_indices, color_values, sizeof(color_indices) / sizeof(color_indices[0])));
 
   delay(3000);
 }
